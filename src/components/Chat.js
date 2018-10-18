6539468be218acd33ce6444af54391c30d3f2cbd
Lukas Stateczny <statecznylukas@gmail.com>
2018-10-18 04:37:54 +0000

    added ecosystem to my personal fork

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,6 +2,7 @@ import React from "react"
 import io from "socket.io-client"
 import {Paper, Button, TextField, ExpansionPanel, ExpansionPanelSummary, Typography, ExpansionPanelDetails} from '@material-ui/core'
 import axios from 'axios'
+import _ from 'lodash'
 // import ExpandMoreIcon from '@material-ui/icons/ExpandMoreIcon';
 
 import './Chat.css'
@@ -12,10 +13,10 @@ class Chat extends React.Component{
 
         this.state = {
             expanded: null,
-            clients: [],
-            id:'',
-            username: 'Lukas',
+            ids: [],
+            // id: '',
             recipient: '',
+            sender: '',
             message: '',
             messages: []
         };
@@ -34,69 +35,101 @@ class Chat extends React.Component{
             addId(data);
         })
 
-        this.socket.on('allClients', function(data) {
-            addAllClients(data)
-        })
+        const addId = (data) => {
+            axios.get(`http://18.191.254.197/getprofile${data.url}`)
+            .then((user) => {
+                let username = user.data.firstName + ' ' + user.data.lastName
+                let id = data.id
 
+                let clients = data.clients
+                for(let i = 0; i < clients.length; i++) {
+                    if(clients[i] === id) {
+                        let client = {
+                            id: id,
+                            username: username
+                        }
+                        this.socket.emit('clients-to-server', client)
+                        this.socket.on('server-to-client', function(data) {
+                            setIDS(data)
+                        })
+                    }
+                }
+            })
+        }
 
-        const addMessage = data => {
-            console.log(data);
-            this.setState({messages: [...this.state.messages, data]});
-            console.log(this.state.messages);
-        };
-
-        const addId = data => {
-            this.setState({id: data})
+        const setIDS = data => {
+            this.setState({ids: _.uniqBy(data, 'username')})
         }
 
         const removeClient = data => {
-            this.state.clients.map((client) => {
-                if(client === data) {
-                    this.state.clients.splice(client, 1)
+            this.state.ids.map((client, i) => {
+                if(client.id === data) {
+                    this.state.ids.splice(i, 1)
                 };
             })
             this.setState({ state: this.state });
         }
 
-        const addAllClients = data => {
-            this.setState({clients: data});
-        }
-
         this.sendMessage = ev => {
             ev.preventDefault();
+            let author = ''
+            // this.setState({sender: this.socket.id})
+            for (let i = 0; i < this.state.ids.length; i++) {
+                if (this.state.ids[i].id === this.socket.id) {
+                    author = this.state.ids[i].username
+                    console.log(author)
+                }
+            }
             this.socket.emit('SEND_MESSAGE', {
+                sender: this.socket.id,
                 recipient: this.state.recipient,
-                author: this.state.username,
+                author: author,
                 message: this.state.message
             })
             this.setState({message: ''});
+            console.log('Current socket id', this.socket.id)
+            console.log('These are clients', this.state.ids)
         }
+
+        const addMessage = data => {
+            this.setState({messages: [...this.state.messages, data]});
+        };
 
         this.handleChange = panel => (event, expanded) => {
             this.setState({
               expanded: expanded ? panel : false,
             });
         };
+
+        // this.handleRecipient = e => {
+        //     console.log(e.target.key)
+        // } 
     }
     
     render() {
         return (      
             <div>
             <Paper>
-                {this.state.clients.map((client, i) => {
+                {this.state.ids.map((client, i) => {
                     return(     
-                    <div key={client} className='chat-container' onClick={()=> {this.setState({recipient: client})}}>
+                    <div key={client.id} data-tag={client.id} className='chat-container' onClick={()=> {this.setState({recipient: client.id})}}>
                     <ExpansionPanel expanded={this.state.expanded === i} onChange={this.handleChange(i)}>
                         <ExpansionPanelSummary>
-                        <Typography>{client}</Typography>
+                        <Typography>{client.username}</Typography>
                         </ExpansionPanelSummary>
                         <ExpansionPanelDetails>
                             <Typography>
-                                <div key={client} className="messages">
+                                <div className="messages">
                                     {this.state.messages.map(message => {
-                                        if(client === this.state.id) {
+                                        console.log(message.sender)
+                                        if(message.sender !== message.recipient && this.socket.id === message.recipient && client.id === message.sender) {
                                             return (
-                                                <div>{message.author}: {message.message}</div>
+                                                <div><span className='author'>{message.author}:</span> {message.message}</div>
+                                            )
+                                        } 
+                                        else if(message.sender === message.recipient && client.id === message.sender) {
+                                            return (
+                                                <div><img src='https://vignette.wikia.nocookie.net/polandball/images/6/69/Forever-alone-400x400.png/revision/latest?cb=20160311230442'/></div>
                                             )
                                         }
                                     })}
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var socket = require('socket.io');
+var _ = require('lodash')
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 
 var server = app.listen(8080, function(){
     console.log('server is running on port 8080')
@@ -12,20 +14,40 @@ var server = app.listen(8080, function(){
 
 var io = socket(server);
 
+var arrOfClients = []
+
 
 io.on('connection', (socket) => {
+    let url = socket.handshake.headers.referer.match(/(\?.*&)/)[0].split('&')[0]
+    console.log(url)
+    console.log(socket.id)
 
-    io.emit('client', socket.id)
-    console.log(Object.keys(socket.nsp.connected))
-    io.emit('allClients', Object.keys(socket.nsp.connected))
-
-    socket.on('SEND_MESSAGE', function(data){
-        console.log(data.recipient)
-        io.to(`${data.recipient}`).emit('RECEIVE_MESSAGE', data);
-        io.to(`${socket.id}`).emit('RECEIVE_MESSAGE', data);
-    })
+    io.emit('client', {id: socket.id, url: url, clients: Object.keys(socket.nsp.connected)})
+    
     socket.on('disconnect', function() {
         console.log('Got disconnected!', socket.id)
+        for (var i = 0; i < arrOfClients.length; i++) {
+            if (arrOfClients[i].id === socket.id) {
+                arrOfClients.splice(i, 1)
+            }
+        }
+        console.log(arrOfClients)
         io.emit('disconnected', socket.id)
      });
+
+    socket.on('clients-to-server', async function(data) {
+        console.log('This is data from front-end',data)
+        await arrOfClients.push(data);
+        var result = _.uniqBy(arrOfClients, await function(e) {
+            return e.username;
+        });
+        arrOfClients = result
+        io.emit('server-to-client', arrOfClients)
+        console.log('These are all connected clients', arrOfClients)
+    })
+
+    socket.on('SEND_MESSAGE', function(data){
+        io.to(`${data.recipient}`).emit('RECEIVE_MESSAGE', data);
+        io.to(`${data.sender}`).emit('RECEIVE_MESSAGE', data);
+    })
 });
